a5a4b2802191a9fcc7f19dc1dddf828a68eef56a
Yuddha Wastu <116266779+ILeszPlayer@users.noreply.github.com>
2026-06-06 21:21:18 +0700

    stb

--- src/main/java/com/example/smartcommunity/model/Admin.java
+++ src/main/java/com/example/smartcommunity/model/Admin.java
@@ -1,8 +1,13 @@
 package com.example.smartcommunity.model;
 
-import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 
 @Entity
 @DiscriminatorValue("ADMIN")
